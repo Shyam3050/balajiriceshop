ff77b223eb34e469a5b480424f057fe8ea224594
Shyam3050 <nimmanashyam3050@gmail.com>
2023-06-04 20:50:37 +0530

    logo added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
-import styles from "./css/Navbar.module.css"
-import {TfiMenu} from "react-icons/tfi"
+import styles from "./css/Navbar.module.css";
+import { TfiMenu } from "react-icons/tfi";
+import logo from "../../assets/balajiriceshoplogo.png";
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <div className={styles.navbar}>
-        <TfiMenu/>
-        <div>Logo</div>
-        <div></div>
+        <TfiMenu />
+       <img src={logo} alt="" srcset="" />
+       <p></p>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
